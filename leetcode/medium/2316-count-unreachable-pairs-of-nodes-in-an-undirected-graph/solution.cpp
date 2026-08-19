1f008cef552fbe75bf881b628f6a629a84905118
sahil kumar <129476952+Sahilkumar121@users.noreply.github.com>
2026-08-19 22:22:28 +0530

    ✅ Count Unreachable Pairs of Nodes in an Undirected Graph | Medium | C++ | Runtime: 46 ms (69.25%) | Memory: 151 MB | 2026-08-19

--- leetcode/medium/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/solution.cpp
+++ leetcode/medium/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/solution.cpp
@@ -1,69 +1,57 @@
 class Solution {
 public:
-    int find(int i, vector<int> &parent)
-    {
-        if (i == parent[i])
-        {
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int i) {
+        if (i == parent[i]) {
             return i;
         }
 
-        return parent[i] = find(parent[i], parent);
+        return parent[i] = find(parent[i]);
     }
 
-    void union_find(int x, int y, vector<int> &parent, vector<int> &rank)
-    {
-        int x_parent = find(x, parent);
-        int y_parent = find(y, parent);
+    void union_find(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
 
-        if (x_parent != y_parent)
-        {
-            if (rank[x_parent] > rank[y_parent])
-            {
+        if (x_parent != y_parent) {
+            if (rank[x_parent] > rank[y_parent]) {
                 parent[y_parent] = x_parent;
-            }
-            else if (rank[y_parent] > rank[x_parent])
-            {
+            } else if (rank[y_parent] > rank[x_parent]) {
                 parent[x_parent] = y_parent;
             }
 
-            else
-            {
+            else {
                 parent[y_parent] = x_parent;
                 rank[x_parent]++;
             }
         }
     }
-
-    long long countPairs(int n, vector<vector<int>> &edges)
-    {
-
-        vector<int> parent(n, -1);
-        vector<int> rank(n, 0);
-        for (int i = 0; i < n; ++i)
-        {
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; ++i) {
             parent[i] = i;
         }
 
         // create union of edges
-        for (const auto &pairs : edges)
-        {
-            union_find(pairs.front(), pairs.back(), parent, rank);
+        for (const auto& pairs : edges) {
+            union_find(pairs.front(), pairs.back());
         }
 
         // find components
         unordered_map<int, int> m;
-        for (int i = 0; i < n; i++)
-        {
-            m[parent[i]]++;
+        for (int i = 0; i < n; i++) {
+            m[find(i)]++;
         }
 
-        int remaining = n;
+        long long remaining = n;
         long long count = 0;
 
-        for (const auto &pair : m)
-        {
-            count += pair.second * (remaining - pair.second);
-            remaining -= pair.second;
+        for (const auto& it : m) {
+            count += (it.second) * (remaining - it.second);
+            remaining -= it.second;
         }
 
         return count;
