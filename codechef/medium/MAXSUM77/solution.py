a6bf2ee8369dc02b3666dcf63d5bb0a8218b8f2f
sahil kumar <129476952+Sahilkumar121@users.noreply.github.com>
2026-09-02 21:06:31 +0530

    ✅ MAXSUM77 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/MAXSUM77/solution.py
+++ codechef/medium/MAXSUM77/solution.py
@@ -1,1 +1,41 @@
-print("Hello, world!")
+import sys
+from sys import maxunicode
+
+
+def solve(it) -> str:
+    # write code here
+    n = int(next(it))
+    k = int(next(it))
+
+    nums: list[int] = []
+    for _ in range(n):
+        num = int(next(it))
+        nums.append(num)
+
+    window = n - k
+    current_sum = sum(nums[:window])
+    max_sum = current_sum
+
+    for i in range(1, window + 1):
+        window_sum = nums[i] + nums[i+1]
+        max_sum = max(max_sum, window_sum)
+
+    return str(max_sum)
+
+
+def main():
+    input_data = sys.stdin.read().split()
+    it = iter(input_data)
+
+    # t = 1
+    t = int(next(it)) # comment it when no test case given
+
+    answer: list[str] = []
+    for _ in range(t):
+        answer.append(solve(it))
+
+    sys.stdout.write("\n".join(answer) + "\n")
+
+
+if __name__ == "__main__":
+    main()
