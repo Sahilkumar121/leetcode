cd3cf3df78e47213e3f2111390eba5949d7024f6
sahil kumar <129476952+Sahilkumar121@users.noreply.github.com>
2026-09-08 21:45:39 +0530

    ✅ Find The Original Array of Prefix Xor | Medium | Python | Runtime: 28 ms (53.82%) | Memory: 37 MB | 2026-09-08

---
+++ leetcode/medium/2433-find-the-original-array-of-prefix-xor/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        n = len(pref)
+
+        ans: list[int] = [0] * n
+        ans[0] = pref[0]
+
+        for i in range(1, n):
+            ans[i] = pref[i - 1] ^ pref[i]
+
+        return ans
